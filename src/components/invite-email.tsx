b7b81eb13f1564bb5a32a0c1816a8f12bb3ac80d
Christian White <christianbwhite@gmail.com>
2023-08-14 01:47:21 -0500

    updated email template

--- src/components/invite-email.tsx
+++ src/components/invite-email.tsx
@@ -8,7 +8,9 @@ const InviteEmail: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
 }) => (
   <div>
-    <h1>Welcome, {firstName}!</h1>
+    <h1>Welcome to Wordle Teams!</h1>
+    <div>You have been invited to join a team.</div>
+    <div>Head to wordleteams.vercel.app to get started!</div>
   </div>
 );
 
