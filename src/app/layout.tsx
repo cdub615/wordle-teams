a9e1ad63ca7f75082ad04d08aa96210603b9c31f
Christian White <christianbwhite@gmail.com>
2024-02-02 17:10:51 -0600

    show maintenance in root layout based on edge config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
+import Maintenance from '@/components/maintenance'
 import ThemeProvider from '@/components/theme-provider'
 import { Toaster } from '@/components/ui/sonner'
-import { cn, getSession } from '@/lib/utils'
+import { cn } from '@/lib/utils'
 import { Analytics } from '@vercel/analytics/react'
+import { get } from '@vercel/edge-config'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import type { Metadata } from 'next'
 import { AxiomWebVitals } from 'next-axiom'
 import { Inter } from 'next/font/google'
-import { redirect } from 'next/navigation'
 import './globals.css'
 
 export const dynamic = 'force-dynamic'
@@ -20,13 +21,16 @@ export const metadata: Metadata = {
     'Wordle Teams lets you compete with friends by tracking and comparing your Wordle scores, adding a competitive edge to the popular word-guessing game. Stay ahead of the competition, enjoy friendly rivalry, and prove your Wordle mastery with this exciting score-tracking app. Revive the Wordle craze and bring your A-game to the ultimate word-guessing showdown with Wordle Teams!',
 }
 
-export default async function RootLayout({children}: {children: React.ReactNode}) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const maintenance = await get<boolean>(`maintenance_${process.env.ENVIRONMENT}`)
+  console.log(`maintenance value from key maintenance_${process.env.ENVIRONMENT}: ${maintenance}`)
+
   return (
     <html lang='en' suppressHydrationWarning>
       <AxiomWebVitals />
       <body className={rootClasses}>
         <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          <main>{children}</main>
+          <main>{maintenance ? <Maintenance /> : children}</main>
           <Toaster />
         </ThemeProvider>
         <Analytics />
