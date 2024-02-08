eb0cf210babc1c53d9789e8d14542f18a63168fc
Christian White <christianbwhite@gmail.com>
2024-02-07 18:34:45 -0600

    used faster get session in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
 import Welcome from '@/components/welcome'
+import { createClient } from '@/lib/supabase/server'
+import { getSession } from '@/lib/utils'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { createClient } from '../lib/supabase/server'
 
 export default async function Home() {
   const supabase = createClient(cookies())
-  const { data } = await supabase.auth.getUser()
-  const { user } = data
-  if (user) {
-    const initials = `${user.user_metadata.firstName[0].toLocaleLowerCase()}${user.user_metadata.lastName[0].toLocaleLowerCase()}`
+  const session = await getSession(supabase)
+  if (session?.user) {
+    const { user_metadata } = session.user
+    const initials = `${user_metadata.firstName[0].toLocaleLowerCase()}${user_metadata.lastName[0].toLocaleLowerCase()}`
     redirect(`/${initials}`)
   }
   return <Welcome />
