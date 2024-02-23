a2144981300ea89280e86d7a1411a4e520d8ebb4
Christian White <christianbwhite@gmail.com>
2024-02-23 15:37:56 -0600

    updated pending state for buttons with icons rather than labels, to replace icon with spinner

--- src/components/submit-button.tsx
+++ src/components/submit-button.tsx
@@ -22,8 +22,8 @@ export default function SubmitButton({ label, children, ...props }: SubmitButton
         disabled={pending}
         {...props}
       >
-        {pending && (
-          <svg className='animate-spin h-5 w-5 mr-3' viewBox='0 0 24 24'>
+        {pending ? (
+          <svg className='animate-spin h-5 w-5' viewBox='0 0 24 24'>
             <circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4' />
             <path
               className='opacity-75'
@@ -31,8 +31,9 @@ export default function SubmitButton({ label, children, ...props }: SubmitButton
               d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
             />
           </svg>
+        ) : (
+          children
         )}
-        {children}
       </Button>
     )
 
