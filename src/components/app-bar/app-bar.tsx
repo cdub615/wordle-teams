35829f733554041192bb13c6aea03d204786a0fa
Christian White <christianbwhite@gmail.com>
2024-02-26 15:58:16 -0600

    reduced padding in app bar for mobile

--- src/components/app-bar/app-bar.tsx
+++ src/components/app-bar/app-bar.tsx
@@ -24,7 +24,7 @@ export default async function AppBar() {
 
   return (
     <header>
-      <div className='flex justify-between p-6'>
+      <div className='flex justify-between px-4 py-2 md:p-6'>
         <div className='flex justify-center items-center'>
           <h1 className='text-2xl md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-600 via-green-500 to-yellow-400 dark:from-green-600 dark:via-green-300 dark:to-yellow-400'>
             Wordle Teams
