e4fdca9f17232f15f705e7688788f4f634d5bb9f
Christian White <christianbwhite@gmail.com>
2024-02-26 20:53:58 -0600

    dont't continue logged page if not valid params

--- src/app/[initials]/[teamId]/[month]/page.tsx
+++ src/app/[initials]/[teamId]/[month]/page.tsx
@@ -6,15 +6,17 @@ import { redirect } from 'next/navigation'
 export default async function Page({ params }: { params: { initials: string; teamId: string; month: string } }) {
   const { initials, teamId: teamIdString, month } = params
   if (!validParams(initials, teamIdString, month)) redirect('/')
-  const teamId = Number.parseInt(teamIdString)
+  else {
+    const teamId = Number.parseInt(teamIdString)
 
-  return (
-    <div className='p-2 grid gap-2 @md:grid-cols-3 @md:p-12 @md:gap-6'>
-      <ActionButtons classes={'@md:col-span-3'} initials={initials} teamId={teamId} month={month} />
-      <ScoresTable classes={'@md:col-span-3'} teamId={teamId} month={month} />
-      <CurrentTeam initials={initials} teamId={teamId} />
-      <MyTeams />
-      <ScoringSystem teamId={teamId} classes={'@md:row-span-3'} />
-    </div>
-  )
+    return (
+      <div className='p-2 grid gap-2 @md:grid-cols-3 @md:p-12 @md:gap-6'>
+        <ActionButtons classes={'@md:col-span-3'} initials={initials} teamId={teamId} month={month} />
+        <ScoresTable classes={'@md:col-span-3'} teamId={teamId} month={month} />
+        <CurrentTeam initials={initials} teamId={teamId} />
+        <MyTeams />
+        <ScoringSystem teamId={teamId} classes={'@md:row-span-3'} />
+      </div>
+    )
+  }
 }
