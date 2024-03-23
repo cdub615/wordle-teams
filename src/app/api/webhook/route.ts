f6b24c3c6faaa0118da391d37950d8bcf88cee61
Christian White <christianbwhite@gmail.com>
2024-03-23 14:31:43 -0500

    on insert to webhook_events, call process event webhook

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,7 +1,6 @@
 import { processWebhookEvent, storeWebhookEvent } from '@/app/me/actions'
 import { webhookHasMeta } from '@/lib/typeguards'
 import { WebhookEvent } from '@/lib/types'
-import { log } from 'next-axiom'
 
 export async function POST(request: Request) {
   if (!process.env.LEMONSQUEEZY_WEBHOOK_SECRET) {
@@ -9,50 +8,46 @@ export async function POST(request: Request) {
       status: 500,
     })
   }
-
-  let rawBody: string
-
-  try {
-    // First, make sure the request is from Lemon Squeezy.
-    rawBody = await request.text()
-    const secret = process.env.LEMONSQUEEZY_WEBHOOK_SECRET
-
-    // const hmac = crypto.createHmac('sha256', secret)
-    // const digest = Buffer.from(hmac.update(rawBody).digest('hex'), 'utf8')
-    // const signature = Buffer.from(request.headers.get('X-Signature') ?? '', 'utf8')
-    const signature = request.headers.get('X-Signature') ?? ''
-
-    if (secret !== signature) {
-      return new Response('Invalid signature', { status: 400 })
-    }
-    // if (!crypto.timingSafeEqual(digest, signature)) {
-    //   return new Response('Invalid signature', { status: 400 })
-    // }
-  } catch (error: any) {
-    log.error(error)
-    return new Response('Could not validate signature', { status: 500 })
+  if (!process.env.SUPABASE_WEBHOOK_SECRET) {
+    return new Response('Supabase Webhook Secret not set in .env', {
+      status: 500,
+    })
   }
 
-  const data = JSON.parse(rawBody) as unknown
-
-  // Type guard to check if the object has a 'meta' property.
-  if (webhookHasMeta(data)) {
-    const webhookEventId = await storeWebhookEvent(data.meta.event_name, data)
-    if (!webhookEventId) {
-      return new Response('Failed to store webhook event', { status: 500 })
-    }
-
-    const webhookEvent = new WebhookEvent(
-      webhookEventId,
-      data.meta.custom_data.user_id,
-      data.meta.event_name,
-      data
-    )
-    // Non-blocking call to process the webhook event.
-    void processWebhookEvent(webhookEvent)
-
-    return new Response('OK', { status: 200 })
+  const rawBody = await request.text()
+  const { fromLemonSqueezy, fromSupabase } = validateSignature(request.headers.get('X-Signature') ?? '')
+  if (!fromLemonSqueezy && !fromSupabase) {
+    return new Response('Invalid signature', { status: 400 })
   }
 
-  return new Response('Data invalid', { status: 400 })
+  if (fromLemonSqueezy) {
+    const data = JSON.parse(rawBody) as unknown
+
+    // Type guard to check if the object has a 'meta' property.
+    if (webhookHasMeta(data)) {
+      const webhookEventId = await storeWebhookEvent(data.meta.event_name, data)
+      if (!webhookEventId) {
+        return new Response('Failed to store webhook event', { status: 500 })
+      }
+
+      return new Response('OK', { status: 200 })
+    }
+
+    return new Response('Data invalid', { status: 400 })
+  }
+
+  if (fromSupabase) {
+    const webhookEvent = JSON.parse(rawBody) as WebhookEvent
+    await processWebhookEvent(webhookEvent)
+  }
+}
+
+const validateSignature = (signature: string) => {
+  const lemonSqueezySecret = process.env.LEMONSQUEEZY_WEBHOOK_SECRET
+  const supabaseSecret = process.env.SUPABASE_WEBHOOK_SECRET
+
+  const fromLemonSqueezy = signature === lemonSqueezySecret
+  const fromSupabase = signature === supabaseSecret
+
+  return { fromLemonSqueezy, fromSupabase }
 }
