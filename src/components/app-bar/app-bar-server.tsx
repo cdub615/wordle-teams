3e01cf9882dc23e612ed0e824e77123ac6a67e05
Christian White <christianbwhite@gmail.com>
2024-03-25 15:59:28 -0500

    inspecting user since no customer id

--- src/components/app-bar/app-bar-server.tsx
+++ src/components/app-bar/app-bar-server.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/lib/supabase/server'
 import { User } from '@/lib/types'
 import { getSession, getUserFromSession } from '@/lib/utils'
+import { log } from 'next-axiom'
 import { cookies } from 'next/headers'
 import AppBarBase from './app-bar-base'
 
@@ -11,6 +12,7 @@ export default async function AppBarServer() {
   const session = await getSession(supabase)
   if (session) {
     user = getUserFromSession(session)
+    log.info(`User`, user)
   }
 
   return <AppBarBase user={user} />
