ac2933cde97fd11ce5ae6c092e9aec3d65a12cef
Christian White <christianbwhite@gmail.com>
2024-04-06 13:58:29 -0500

    fixing db types

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -404,7 +404,7 @@ export type Database = {
         Args: {
           name: string
         }
-        Returns: unknown
+        Returns: string[]
       }
       get_size_by_bucket: {
         Args: Record<PropertyKey, never>
