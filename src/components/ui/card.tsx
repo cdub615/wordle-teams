f6a398ac47e74777ce7199334b6e4f03f28e207f
Christian White <christianbwhite@gmail.com>
2024-04-22 15:08:25 -0500

    trying reducing px in all cards for mobile

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -23,7 +23,7 @@ const CardHeader = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex flex-col space-y-1.5 p-6", className)}
+    className={cn("flex flex-col space-y-1.5 px-4 py-6 md:p-6", className)}
     {...props}
   />
 ))
