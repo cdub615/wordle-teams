93e039af38719ec98ea0ea5d7a207b5c483d6f4b
Christian White <christianbwhite@gmail.com>
2024-04-22 11:31:30 -0500

    set/show boards using state, handle no board or logged in user's board for today not submitted

--- src/components/app-grid-items/team-boards.tsx
+++ src/components/app-grid-items/team-boards.tsx
@@ -5,20 +5,49 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '@/components/ui/carousel'
 import { useTeams } from '@/lib/contexts/teams-context'
 import { cn } from '@/lib/utils'
-import { isSameDay } from 'date-fns'
-import { useState } from 'react'
+import { isSameDay, isToday } from 'date-fns'
+import { useEffect, useState } from 'react'
 import WordleBoard from '../wordle-board'
 
 export default function TeamBoards({ classes }: { classes?: string }) {
   const { teams, teamId, user } = useTeams()
   const team = teams.find((t) => t.id === teamId)!
   const [date, setDate] = useState<Date | undefined>(new Date())
-  const boards = team.players.map((p) => p.scores.filter((s) => isSameDay(new Date(s.date), date!))).flat()
+  const getBoardsForDate = (selectedDate: Date) => {
+    const boards = team.players.map((p) => {
+      const score = p.scores.find((s) => isSameDay(new Date(s.date), selectedDate))
+      const board: Board = {
+        id: score ? score.id.toString() : p.id,
+        exists: !!score,
+        playerId: p.id,
+        playerName: `${p.firstName} ${p.lastName}`,
+        date: new Date(score?.date ?? ''),
+        answer: score?.answer ?? '',
+        guesses: score?.guesses ?? [],
+      }
+      return board
+    })
+    return boards
+  }
+  const shouldShowBoards = (selectedDate: Date) =>
+    !isToday(selectedDate) ||
+    (team.players.find((p) => p.id === user?.id)?.scores.some((s) => isToday(new Date(s.date))) ?? false)
+
+  const [boards, setBoards] = useState(getBoardsForDate(date!))
+  const [showBoards, setShowBoards] = useState(shouldShowBoards(date!))
+
+  useEffect(() => {
+    if (date) {
+      setBoards(getBoardsForDate(date!))
+      setShowBoards(shouldShowBoards(date!))
+    }
+  }, [date])
+
   return (
     <Card className={cn('w-full max-w-md', classes)}>
       <CardHeader>
-        <CardTitle>Wordle Boards</CardTitle>
-        <div className='w-full max-w-md'>
+        <CardTitle>Team Boards</CardTitle>
+        <div className='w-full max-w-md pt-2'>
           <DatePicker date={date} setDate={setDate} playWeekends={team.playWeekends} />
         </div>
       </CardHeader>
@@ -26,15 +55,36 @@ export default function TeamBoards({ classes }: { classes?: string }) {
         <Carousel className='w-full'>
           <CarouselContent>
             {boards.map((b) => (
-              <CarouselItem key={b.id}>
-                <WordleBoard answer={b.answer} guesses={b.guesses} />
+              <CarouselItem key={b.id} className='h-[400px]'>
+                <div className='font-semibold text-center pb-2 h-[24px]'>{b.playerName}</div>
+                {showBoards && b.exists && <WordleBoard answer={b.answer} guesses={b.guesses} />}
+                {showBoards && !b.exists && (
+                  <div className='flex h-full justify-center'>
+                    <p className='pt-[164px] text-muted-foreground'>No board for player on this date</p>
+                  </div>
+                )}
+                {!showBoards && (
+                  <div className='flex h-full justify-center'>
+                    <p className='pt-[164px] text-muted-foreground'>Today's board not yet submitted</p>
+                  </div>
+                )}
               </CarouselItem>
             ))}
           </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
+          <CarouselPrevious className='-left-5 md:-left-4' />
+          <CarouselNext className='-right-5 md:-right-4' />
         </Carousel>
       </CardContent>
     </Card>
   )
+}
+
+type Board = {
+  id: string
+  exists: boolean
+  playerId: string
+  playerName: string
+  date: Date
+  guesses: string[]
+  answer: string
 }
