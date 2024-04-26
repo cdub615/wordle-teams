6265b447abb45e77e2b286cba46b559ab51fc20d
Christian White <christianbwhite@gmail.com>
2024-04-26 13:44:20 -0500

    footer verbiage

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -45,7 +45,7 @@ export default async function Welcome() {
         <Link href='https://twitter.com/wordleteams' className='underline underline-offset-4'>
           X
         </Link>
-        . View source code on{' '}
+        . View source code in{' '}
         <Link href='https://github.com/cdub615/wordle-teams' className='underline underline-offset-4'>
           GitHub
         </Link>
