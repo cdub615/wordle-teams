c74591544d90e00f5d100a2075654232aca0409a
Christian White <christianbwhite@gmail.com>
2024-05-03 14:09:36 -0500

    remove sentry test stuff

--- src/components/action-buttons/action-buttons.tsx
+++ src/components/action-buttons/action-buttons.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { BoardEntryButton } from './board-entry-button'
 import MonthDropdown from './month-dropdown/month-dropdown'
-import SentryClientTest from './sentry-test'
 import TeamsDropdown from './teams-dropdown/teams-dropdown'
-import * as Sentry from '@sentry/nextjs'
 
 type ActionButtonProps = {
   userId: string
@@ -18,20 +15,7 @@ export default async function ActionButtons({ userId, classes }: ActionButtonPro
       <div className='flex-grow'>
         <TeamsDropdown />
       </div>
-      <SentryServerTest />
-      <SentryClientTest />
       <BoardEntryButton userId={userId} />
     </div>
-  )
-}
-export async function SentryServerTest() {
-  const handleSubmit = async (formData: FormData) => {
-    'use server'
-    Sentry.captureException(new Error('testing Sentry from server component'))
-  }
-  return (
-    <form action={handleSubmit}>
-      <Button type='submit'>Sentry Server Test</Button>
-    </form>
   )
 }
