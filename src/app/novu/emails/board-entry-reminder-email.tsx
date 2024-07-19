8137d77ac5a87d2cb21c42a83cabfbe24cfa5e9c
Christian White <christianbwhite@gmail.com>
2024-07-19 14:28:16 -0500

    fixing email markup

--- src/app/novu/emails/board-entry-reminder-email.tsx
+++ src/app/novu/emails/board-entry-reminder-email.tsx
@@ -18,8 +18,8 @@ type BoardEntryReminderProps = ControlSchema & PayloadSchema
 
 export const BoardEntryReminderEmail = ({
   previewText,
-  worldleTeamsImg,
-  wtIconImg,
+  wordleTeamsImage,
+  wtIconImage,
   email,
   firstName,
 }: BoardEntryReminderProps) => {
@@ -31,18 +31,18 @@ export const BoardEntryReminderEmail = ({
         <Body className='bg-white my-auto mx-auto font-sans'>
           <Container className='rounded my-[40px] mx-auto p-[20px] max-w-xl'>
             <Section className='mt-[32px]'>
-              <Img src={worldleTeamsImg} width='400' alt='Wordle Teams' className='my-0 mx-auto rounded' />
+              <Img src={wordleTeamsImage} width='400' alt='Wordle Teams' className='my-0 mx-auto rounded' />
             </Section>
             <Heading className='text-black text-4xl font-normal text-center p-0 my-[30px] mx-0'>
               Reminder to enter your Wordle board into <strong>Wordle Teams</strong>
             </Heading>
             <Text className='text-black text-xl leading-2'>Hello {firstName},</Text>
             <Text className='text-black text-xl mb-6'>
-              It looks like you have not yet entered your Wordle board for today. Don't miss out on those potential
-              points!
+              It looks like you have not yet entered your Wordle board for today. Don&apos;t miss out on those
+              potential points!
             </Text>
             <Text className='text-black text-xl'>Best of luck!</Text>
-            <Img className='rounded-full' src={wtIconImg} width='32' height='32' />
+            <Img className='rounded-full' src={wtIconImage} width='32' height='32' />
             <Text className='text-black text-lg'>Wordle Teams</Text>
             <Hr className='border border-solid border-[#eaeaea] my-[26px] mx-0 w-full' />
             <Text className='text-[#858585] text-sm leading-[24px]'>
