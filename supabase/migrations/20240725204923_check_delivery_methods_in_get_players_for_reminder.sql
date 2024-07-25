4272a8c4c7e593127bdca91f2e3da254aadde7f5
Christian White <christianbwhite@gmail.com>
2024-07-25 15:50:29 -0500

    ensure player has delivery method(s) enabled in get players for reminder

---
+++ supabase/migrations/20240725204923_check_delivery_methods_in_get_players_for_reminder.sql
@@ -0,0 +1,12 @@
+CREATE OR REPLACE FUNCTION get_players_for_reminder()
+RETURNS SETOF players
+LANGUAGE sql
+AS $$
+  SELECT *
+  FROM players
+  WHERE reminder_delivery_time <= (CURRENT_TIMESTAMP AT TIME ZONE time_zone)::time
+    AND reminder_delivery_time >= ((CURRENT_TIMESTAMP - INTERVAL '1 hour') AT TIME ZONE time_zone)::time
+    AND (last_board_entry_reminder IS NULL OR last_board_entry_reminder < DATE_TRUNC('day', CURRENT_TIMESTAMP AT TIME ZONE time_zone))
+    AND reminder_delivery_methods IS NOT NULL AND array_length(reminder_delivery_methods, 1) > 0
+  ORDER BY last_board_entry_reminder ASC;
+$$;
